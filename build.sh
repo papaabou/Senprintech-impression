616c8a705c5b69d7e1d8fa93cedd32d5d0e31ecf
papaaboumabye25@gmail.com <ton.email@example.com>
2026-05-24 18:24:15 +0200

    Seed catalogue during Render build

--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@ python -m pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate --no-input
+python manage.py seed_senprintech
